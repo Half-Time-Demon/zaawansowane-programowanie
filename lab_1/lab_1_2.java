4fe8a16afbb9be7f6e0e16cf3168651a79ae68e5
Half-Time-Demon <72053859+Half-Time-Demon@users.noreply.github.com>
2022-10-15 00:26:02 +0300

    Create lab_1_2.java

---
+++ lab_1/lab_1_2.java
@@ -0,0 +1,13 @@
+/**
+ * lab_1_2
+ */
+public class lab_1_2 {
+    public static void main(String[] args) {
+        char[] metoda ={'D','a','n','i','e','l','-','T','r','o'};
+        for(int i=0;i<=metoda.length;i++){
+            System.out.println(metoda[i]);
+
+        }
+
+    }
+}
