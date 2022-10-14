d2c16e6ef8c65af990dcb1eee1bdfd351995babf
Half-Time-Demon <72053859+Half-Time-Demon@users.noreply.github.com>
2022-10-15 00:25:03 +0300

    Create lab_1_1.java

---
+++ lab_1/lab_1_1.java
@@ -0,0 +1,20 @@
+/**
+ * lab_1_1
+ */
+public class lab_1_1 {
+
+    public static void main(String[] args) {
+        String imie_i_nazwisko="Daniel Trojko";
+        int waga=76;
+        float wzrost=1.72f;
+        boolean czy_studiuje=true;
+        char  rok_studiow='2';
+        long pesel=48494561895648L;
+
+        System.out.println("Imie i Nazwisko: " + imie_i_nazwisko);
+        System.out.println("Waga: " + waga);
+        System.out.println("Wzrost: " +  wzrost);
+        System.out.println("Czy studiuje: " + czy_studiuje);
+        System.out.println("Rok studiow: " + rok_studiow);
+        System.out.println("PASEL: " + pesel);
+    }
