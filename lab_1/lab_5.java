ce7932d8f56ac48f9ffc59ac49d6301893f31f41
Half-Time-Demon <72053859+Half-Time-Demon@users.noreply.github.com>
2022-10-15 00:28:45 +0300

    Create lab_5.java

---
+++ lab_1/lab_5.java
@@ -0,0 +1,41 @@
+/**
+ * lab_1_5
+ */
+public class lab_5 {
+    public static void main(String[] args){
+
+		
+    int metoda[] = new int[]{1, 2, 3, 3, 3};
+
+		for (int i=0; i<metoda.length; i++){
+			
+
+			if (metoda[i] == 1){
+				System.out.println("Tak"); break;
+			}
+			else if(metoda[i] == 2){
+				System.out.println("Nie"); break;
+			}
+			else if(metoda[i] == 3){
+				System.out.println("Nie wiem"); break;
+			}
+			else{
+				System.out.println("???"); break;
+			}
+		}
+
+		for (int i=0; i<metoda.length; i++){
+			switch(metoda[i]){
+
+			case 1:
+				System.out.println("Tak"); break;
+			case 2:
+				System.out.println("Nie"); break;
+			case 3:
+				System.out.println("Nie wiem"); break;
+			default:
+				System.out.println("???"); break;
+			}
+		}
+	}
+}
